fc91e8f1613f000713d22ac1fe7fef112083cbe3
AxeAnton <axespv@gmail.com>
2022-03-24 10:02:07 +0300

    Домашнее задание про школу

---
+++ src/ru/itmo/hw/hw6/mouses/task3/mouse/Cat.java
@@ -0,0 +1,32 @@
+package ru.itmo.hw.hw6.mouses.task3.mouse;
+
+/*public class Cat {*/
+/*
+    // свойства
+    public String name;
+    public int speed;
+    public int weight;
+    public int caught;
+    // инфо о мышах
+    public Mouse[] mouses = new Mouse[100];
+*/
+
+/*
+    // метод добавления мыши
+    public void addMouse(Mouse mouse) {
+        for (int i = 0; i < mouses.length; i++) {
+            if (mouses[i] != 0) {
+                mouses[i] = mouse;
+                return;
+            }
+        }
+     }
+*/
+/*
+
+
+    // метод
+    public int getSpeed
+
+}
+*/
---
+++ src/ru/itmo/hw/hw7/task3/Application.java
@@ -0,0 +1,19 @@
+//package ru.itmo.hw.hw6.mouses;
+//
+//import ru.itmo.hw.hw6.mouses.mouse.Cat;
+//import ru.itmo.hw.hw6.mouses.mouse.Mouse;
+//
+//public class Application {
+//    public static void main(String[] args) {
+//        Mouse mouse1 = new Mouse();
+//        mouse1.speedMouse = 10;
+//
+//
+//
+//        Cat cat1 = new Cat();
+//        cat1.name = "Кот1";
+//        cat1.speed = 15;
+//        cat1.weight = 18;
+//
+//    }
+//}
